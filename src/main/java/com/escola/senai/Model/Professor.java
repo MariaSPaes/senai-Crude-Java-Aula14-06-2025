f9f87e080aec2ced194091e14ad51809068f0a87
MariaSPaes <mariaespaes@gmail.com>
2025-06-28 17:50:53 -0300

    PROFESSOR-CLASSESFEAT-28-06

--- src/main/java/com/escola/senai/Model/Professor.java
+++ src/main/java/com/escola/senai/Model/Professor.java
@@ -1,4 +1,47 @@
 package com.escola.senai.Model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Professor {
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String telefone;
 }
